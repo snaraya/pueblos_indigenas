5c1956b33e44b994b5b05f6466887ad51f33c2b0
Soledad Araya <snaraya@uc.cl>
2022-06-10 13:33:57 -0400

    codigo

--- 01-graficos.R
+++ 01-graficos.R
@@ -8,4 +8,14 @@ scales::show_col(paleta)
 df <- read_rds("data/nombre_pueblo_indigena.rds")
 
 df <- df %>% 
-  group_by()
+  group_by(region, nombre_pueblo, sexo) %>% 
+  summarise(n = sum(n)) %>% 
+  arrange(nombre_pueblo, n)
+
+df %>% 
+  ggplot(aes(x = nombre_pueblo, y = n, fill = nombre_pueblo)) +
+  geom_col(width = 0.5) +
+  scale_y_continuous(limits = c(0,200000)) +
+  scale_fill_manual(values = paleta, guide = "none") +
+  coord_flip() +
+  facet_grid(cols = vars(sexo), scales = "free")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# pueblos_indigenas
+# Gráfico: Pueblos Indígenas
+
+La base de datos es de acceso público en la página del Servicio Electoral. Los puedes tener directamente desde este [enlace](https://www.servel.cl/padron-electoral-pueblos-indigenas/). 
